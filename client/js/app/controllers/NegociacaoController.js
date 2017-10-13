c511ecde2f7450c64b77ee76d6e426ac9fb23e14
Tiago <web2.wplay@gmail.com>
2017-10-13 12:56:52 -0300

    atualiza negociacao controller

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,26 +1,32 @@
 class NegociacaoController {
 	constructor() {
+
 		let $ = document.querySelector.bind(document);
 		this._inputData 		= $('#data');
 		this._inputQuantidade 	= $('#quantidade');
 		this._inputValor		= $('#valor');
-		Object.freeze(this);
 	}
 
 	adiciona(event) {
+
 		event.preventDefault;
 		
 		let data = new Date(
-				this._inputData.value.split('-');
+				...this._inputData.value
+	                .split('-')
+	                .map((item, indice) => item - indice % 2)
 			);
 
 		this.limpaFormulario();
 
-		return new Negociacao(
-				new Date(data.getDate()), 
-				this._inputQuantidade.value, 
-				this._inputValor.value
-			);
+		let negociacao = new Negociacao(
+			new Date(data.getDate()), 
+			this._inputQuantidade.value, 
+			this._inputValor.value
+		);
+
+		console.log(negociacao);
+
 	}
 
 	limpaFormulario() {
