5601c80d182abbfb8a5242acaaa88deff77addac
Tiago Fuelber <tfuelber@gmail.com>
2017-11-13 23:07:56 -0200

    usa module pattern na classe ConnectionFactory

--- client/js/app/models/ConnectionFactory.js
+++ client/js/app/models/ConnectionFactory.js
@@ -1,45 +1,48 @@
-var stores = ['negociacoes'];
-var version = 4;
-var dbname = 'aluraframe';
-
-class ConnectionFactory {
+var ConnectionFactory = (function() {
 	
-	constructor() {
+	var stores = ['negociacoes'];
+	var version = 4;
+	var dbname = 'aluraframe';
 
-		throw new Error('Nâo é possível instanciar ConnectionFactory');
+	return class ConnectionFactory {
+		
+		constructor() {
+
+			throw new Error('Nâo é possível instanciar ConnectionFactory');
+		}
+
+		static getConnection () {
+
+			return new Promise((resolve, reject) => {
+
+				let openRequest = window.indexedDB.open('aluraframe', version);
+
+				openRequest.onupgradeneeded = e => {
+
+					ConnectionFactory._createStores(e.target.result);
+				};
+
+				openRequest.onsuccess = e => {
+
+					resolve(e.target.result);
+				};
+
+				openRequest.onerror = e => {
+
+					console.log(e.target.error);
+					reject(e.target.error.name);
+				};
+			});
+		}
+
+		_createStores(connection) {
+
+			stores.forEach(store => {
+				if (connection.target.result.objectStoreNames.contains(store)) 
+					connection.target.result.deleteObjectStore(store);
+				
+				connection.target.result.createObjectStore(store, { autoIncrement: true});
+			})
+		}
 	}
-
-	static getConnection () {
-
-		return new Promise((resolve, reject) => {
-
-			let openRequest = window.indexedDB.open('aluraframe', version);
-
-			openRequest.onupgradeneeded = e => {
-
-				ConnectionFactory._createStores(e.target.result);
-			};
-
-			openRequest.onsuccess = e => {
-
-				resolve(e.target.result);
-			};
-
-			openRequest.onerror = e => {
-
-				console.log(e.target.error);
-				reject(e.target.error.name);
-			};
-		});
-	}
-
-	_createStores(connection) {
-
-		stores.forEach(store => {
-			if (connection.target.result.objectStoreNames.contains(store)) 
-				connection.target.result.deleteObjectStore(store);
-			
-			connection.target.result.createObjectStore(store, { autoIncrement: true});
-		})
-	}
-}
+})();
